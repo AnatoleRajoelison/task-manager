a96f6c78904a423dbdf144b9c7f6a9f09a9ab048
AnatoleRajoelison <hei.anatole@gmail.com>
2023-06-15 10:55:21 +0300

    Création d'un hook pour interagir avec le Local Storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react';
+
 const useLocalStorage = () => {
+  const [value, setValue] = useState(() => {
+    // C'est pour récupérer la valeur depuis le Local Storage ici
+  });
 
-}
+  useEffect(() => {
+    // C'est pour mettre à jour la valeur dans le Local Storage ici
+  }, [value]);
 
-export {
-  useLocalStorage
-}
+  return [value, setValue];
+};
+
+export { useLocalStorage };
+
